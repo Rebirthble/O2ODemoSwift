14f6b20c0707abb7f264638ae7ff397a02ee6d91
OkawaUki <0k4w4.yuk1@gmail.com>
2016-01-15 12:02:44 +0900

    ボタンが押された時の処理を追加

--- O2ODemoSwift/ViewController.swift
+++ O2ODemoSwift/ViewController.swift
@@ -11,10 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     
     //NotificationManagerのプロパティ作成
-
+    
     
     //ボタンが押された時の処理
-    
+    @IBAction func setLocation(sender: AnyObject) {
+        let locationId = "YOUR_LOCATION_ID"
+        let callback = {(error: NSError?) -> Void in
+            if ((error) != nil) {
+                print("error: \(error?.localizedDescription)")
+            } else {
+                print("no error!")
+            }
+        }
+        //設定したlocationIdをもとに位置情報の検索とプッシュ通知の再設定を行う
+        manager.searchLocation(locationId, callback: callback)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
